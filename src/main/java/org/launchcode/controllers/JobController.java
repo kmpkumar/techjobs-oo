e01d2322c858910276ab612d42d40acdce343e86
Maruthi Korrapati <kmpkumar@gmail.com>
2018-12-27 14:23:33 -0600

    Completed final version.Commit.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -24,7 +25,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job someJob = jobData.findById(id);
+        model.addAttribute("someJob", someJob);
         return "job-detail";
     }
 
@@ -41,7 +43,17 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+            return "new-job";
+        }
+        Employer anEmployer = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location anLocation = jobData.getLocations().findById(jobForm.getLocationId());
+        CoreCompetency anCoreCompetency = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+        PositionType anPositionType = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+
+        Job newJob = new Job(jobForm.getName(),anEmployer,anLocation,anPositionType,anCoreCompetency);
+        jobData.add(newJob);
+        return "redirect:/job?id="+newJob.getId();
 
     }
 }
